7965aa78dfce7708cecafe1eb2e72fd6e9929033
TymanWasTaken <32660892+TymanWasTaken@users.noreply.github.com>
2022-02-14 18:33:11 -0700

    I hate npm

--- src/route.ts
+++ src/route.ts
@@ -1,20 +1,73 @@
 import bodyParser from 'body-parser';
+import compression from 'compression';
 import type { Express, Request, RequestHandler, Response } from 'express';
 import type { OpenAPIV3 } from 'openapi-types';
 
+/**
+ * A user created route. Handlers for methods are defined using functions with the same name as the method.
+ * In order to add options, you must use the {@link RouteOptions} decorator.
+ * @abstract
+ */
 export abstract class Route {
+	/**
+	 * The options of the route.
+	 * @type {IRouteOptions}
+	 */
 	declare options: IRouteOptions;
-	public get?(req: Request, res: Response): Promise<void>;
-	public post?(req: Request, res: Response): Promise<void>;
-	public delete?(req: Request, res: Response): Promise<void>;
-    public patch?(req: Request, res: Response): Promise<void>;
-    public put?(req: Request, res: Response): Promise<void>;
-    public head?(req: Request, res: Response): Promise<void>;
+	/**
+	 * Handler for a GET request on this route.
+	 * @param req The express request object
+	 * @param res The express response object
+	 */
+	public get?(req: Request, res: Response): unknown;
+	/**
+	 * Handler for a POST request on this route.
+	 * @param req The express request object
+	 * @param res The express response object
+	 */
+	public post?(req: Request, res: Response): unknown;
+	/**
+	 * Handler for a DELETE request on this route.
+	 * @param req The express request object
+	 * @param res The express response object
+	 */
+	public delete?(req: Request, res: Response): unknown;
+	/**
+	 * Handler for a PATCH request on this route.
+	 * @param req The express request object
+	 * @param res The express response object
+	 */
+    public patch?(req: Request, res: Response): unknown;
+	/**
+	 * Handler for a PUT request on this route.
+	 * @param req The express request object
+	 * @param res The express response object
+	 */
+    public put?(req: Request, res: Response): unknown;
+	/**
+	 * Handler for a HEAD request on this route.
+	 * @param req The express request object
+	 * @param res The express response object
+	 */
+    public head?(req: Request, res: Response): unknown;
+	/**
+	 * The internal express instance used for this route.
+	 * @type {Express}
+	 */
 	public app: Express;
+
+	/**
+	 * The constructor for a Route. This should only be called by the RouteManager.
+	 * @param app The express instance to use for this route.
+	 * @constructs Route
+	 */
 	public constructor(app: Express) {
 		this.app = app;
 	}
 
+	/**
+	 * The internal method to register this route to the express instance. Should only be called by the RouteManager.
+	 */
     public registerRoute() {
         if (this.get) this.app.get(this.options.path, ...(this.options.middleware ?? []), this.get.bind(this));
         if (this.post) this.app.post(this.options.path, ...(this.options.middleware ?? []), this.post.bind(this));
@@ -25,13 +78,19 @@ export abstract class Route {
     }
 }
 
-// Decorator
+/**
+ * Options for a route. Should be used in the {@link RouteOptions} decorator.
+ */
 export interface IRouteOptions {
 	path: string;
 	middleware?: RequestHandler[];
 	spec: OpenAPIV3.PathItemObject | null;
 }
 
+/**
+ * Decorator for {@link Route} to apply options to it
+ * @param {IRouteOptions} options Options to apply to the route
+ */
 export function RouteOptions(routeOptions: IRouteOptions) {
 	return function <T extends { new (...args: any[]): {} }>(constructor: T) {
 		return class extends constructor {
@@ -40,8 +99,12 @@ export function RouteOptions(routeOptions: IRouteOptions) {
 	};
 }
 
-// Middleware
+/**
+ * Preset middleware to use in your app, can be added to individual routes or in the constructor of {@link RouteManager}
+ * @readonly
+ */
 export const Middleware = {
 	UrlEncoded: bodyParser.urlencoded({ extended: true }),
-	Json: bodyParser.json()
-};
+	Json: bodyParser.json(),
+	Compression: compression()
+} as const;
--- src/routeManager.ts
+++ src/routeManager.ts
@@ -1,25 +1,57 @@
 import bodyParser from 'body-parser';
 import express, { RequestHandler, type Express } from 'express';
 import { promises as fs } from 'fs';
+import { OpenAPIV3 } from 'openapi-types';
 import path from 'path';
 import { Route } from './route';
 
+/**
+ * The manager to manage and register all {@link Route}s.
+ */
 export class RouteManager {
+    /**
+     * The internal express instance used for this route manager. Will be passed to all {@link Route}s registered with this manager.
+     */
 	public app: Express;
+    /**
+     * The path to the directory containing the routes to manage.
+     */
 	public routePath: string;
+    /**
+     * The middleware to use globally for all routes.
+     */
 	public middleware: RequestHandler[];
+    /**
+     * An array of all registered {@link Route}s.
+     */
     public routes: Route[];
+    /**
+     * The OpenAPI specification for all routes and this app.
+     */
+    public spec: OpenAPIV3.Document;
 
-	public constructor(routePath: string, middleware?: RequestHandler[]) {
+    /**
+     * Creates a new {@link RouteManager} instance.
+     * @param routePath The path to the directory containing the routes to manage.
+     * @param spec The OpenAPI specification for this app. Should not include any route-specific specifications.
+     * @param middleware The middleware to use globally for all routes.
+     */
+	public constructor(routePath: string, spec: OpenAPIV3.Document, middleware?: RequestHandler[]) {
 		this.routePath = routePath;
 		this.app = express();
 		this.middleware = middleware ?? [];
 		this.middleware.push(bodyParser.json());
 		this.middleware.push(bodyParser.urlencoded({ extended: true }));
-        this.routes = []
+        this.routes = [];
+        this.spec = spec;
 	}
 
+    /**
+     * Registers all routes in the directory specified by {@link routePath}.
+     * Also loads thier OpenAPI specs and serves the OpenAPI spec at `/openapi.json`.
+     */
 	public async loadRoutes(): Promise<void> {
+        // Load all custom routes
         for await (const file of this.recursiveReadDir(this.routePath)) {
             const routeImport = await import(file) as { default?: typeof Route };
             if (!routeImport.default) {
@@ -35,15 +67,27 @@ export class RouteManager {
                 continue
             }
             instance.registerRoute();
+            if (instance.options.spec) this.spec.paths[instance.options.path] = instance.options.spec;
             this.routes.push(instance);
         }
+        // Add callback for /openapi.json
+        this.app.get('/openapi.json', (req, res) => res.status(200).json(this.spec));
     }
 
+    /**
+     * Starts the server on the specified port.
+     * @param port The port to listen on.
+     */
     public async serve(port: number): Promise<void> {
         this.app.listen(port);
         console.log(`Listening on port ${port}`);
     }
 
+    /**
+     * Recursively reads all files in the specified directory.
+     * @param dir The directory to read recursively.
+     * @private
+     */
 	private async *recursiveReadDir(dir: string): AsyncIterableIterator<string> {
 		const dirents = await fs.readdir(dir, { withFileTypes: true });
 		for (const dirent of dirents) {
